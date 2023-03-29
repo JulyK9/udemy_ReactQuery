3333f6c1df03054bbf0160906cae2db7729840ee
JulyK9 <jkesonjk@gmail.com>
2023-03-29 15:33:34 +0900

    [Section1] 기본 설치 및 적용 세팅, useQuery, useMutation 실습

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,3 +1,5 @@
+import { useQuery, useMutation } from 'react-query';
+
 async function fetchComments(postId) {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
@@ -8,7 +10,7 @@ async function fetchComments(postId) {
 async function deletePost(postId) {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/postId/${postId}`,
-    { method: "DELETE" }
+    { method: 'DELETE' }
   );
   return response.json();
 }
@@ -16,19 +18,81 @@ async function deletePost(postId) {
 async function updatePost(postId) {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/postId/${postId}`,
-    { method: "PATCH", data: { title: "REACT QUERY FOREVER!!!!" } }
+    { method: 'PATCH', data: { title: 'REACT QUERY FOREVER!!!!' } }
   );
   return response.json();
 }
 
 export function PostDetail({ post }) {
+  // console.log('post: ', post); // props로 넘겨지는 데이터 확인
   // replace with useQuery
-  const data = [];
+  // const data = [];
+
+  // 문제점: 다른 포스트를 눌러도 같은 댓글만 나옴
+  // 이유: 동일한 쿼리키(comments)를 사용하고 있어서 리페칭이 일어나지 않음
+  // 접근방법:
+  // => 리페칭할 트리거가 필요(리마운트, 윈도우 리포커싱, useQueary에서 반환되어 수동으로 리페칭)
+  // => 지정된 간격으로 리페칭 자동실행, 변이를 생성한 뒤 쿼리를 무효화시킬 때
+  // 쿼리는 게시물 아이디를 포함하기 때문에 쿼리별로 캐시를 남길 수 있음
+  // 각 게시물에 대한 쿼리에 라벨을 설정해줌 => 쿼리키에 문자열 대신 배열을 전달!
+  // 배열의 첫 번째 요소로 문자열 "comments"를 가지고, 두 번째 요소로 post.id를 가짐
+  // 쿼리 키를 쿼리에 대한 의존성 배열로 취급하기 때문에 쿼리 키가 변경되면
+  // 즉 post.id가 업데이트되면 React Query가 새 쿼리를 생성해서
+  // 개별적인 staleTime과 cacheTime을 가지게 되고 의존성 배열이 다르다면 완전히 다른 것으로 간주하게 됨
+
+  // const { data, isLoading, isError, error } = useQuery('comments', () =>
+  //   fetchComments(post.id)
+  // );
+
+  const { data, isLoading, isError, error } = useQuery(
+    ['comments', post.id],
+    () => fetchComments(post.id)
+  );
+
+  const deleteMutation = useMutation((postId) => deletePost(postId));
+  // useMutation 함수는 객체를 반환함
+
+  const updateMutation = useMutation((postId) => updatePost(postId));
+
+  if (isLoading) return <h2>Loading...</h2>;
+  if (isError)
+    return (
+      <>
+        <h2>Something went wrong...</h2>
+        {/* <p>{error.toString()}</p> */}
+        <p>{error.toString().split(': ')[1]}</p>
+      </>
+    );
+
+  // console.log('data: ', data);
 
   return (
     <>
-      <h3 style={{ color: "blue" }}>{post.title}</h3>
-      <button>Delete</button> <button>Update title</button>
+      <h3 style={{ color: 'blue' }}>{post.title}</h3>
+      {/* <button>Delete</button> <button>Update title</button> */}
+      {/* deleteMutation은 객체를 반환하는데 속성함수인 mutate를 실행 */}
+      <button onClick={() => deleteMutation.mutate(post.id)}>Delete</button>
+      {deleteMutation.isError && (
+        <p style={{ color: 'red' }}>Error deleting the post</p>
+      )}
+      {deleteMutation.isLoading && (
+        <p style={{ color: 'blue' }}>Deleting the post</p>
+      )}
+      {deleteMutation.isSuccess && (
+        <p style={{ color: 'green' }}>Post has (not) been deleted</p>
+      )}
+      <button onClick={() => updateMutation.mutate(post.id)}>
+        Update title
+      </button>
+      {updateMutation.isError && (
+        <p style={{ color: 'red' }}>Error updating the post</p>
+      )}
+      {updateMutation.isLoading && (
+        <p style={{ color: 'blue' }}>Updating the post</p>
+      )}
+      {updateMutation.isSuccess && (
+        <p style={{ color: 'green' }}>Post has (not) been updated</p>
+      )}
       <p>{post.body}</p>
       <h4>Comments</h4>
       {data.map((comment) => (
--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,21 +1,66 @@
-import { useState } from "react";
+import { useEffect, useState } from 'react';
+// import { useQuery } from 'react-query';
+import { useQuery, useQueryClient } from 'react-query';
 
-import { PostDetail } from "./PostDetail";
+import { PostDetail } from './PostDetail';
+
+// api가 100개의 게시물을 반환하기 때문에 10으로 설정(아래 limit 10)
 const maxPostPage = 10;
 
-async function fetchPosts() {
+// async function fetchPosts() {
+async function fetchPosts(pageNum) {
   const response = await fetch(
-    "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0"
+    // 'https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0'
+    `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageNum}`
+    // 'https://jsonplaceholder1111.typicode.com/posts?_limit=10&_page=0' // 오류 유도용
   );
   return response.json();
 }
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
+  // const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
 
+  // prefetchQuery를 통해 nextPage 정보를 사전 페칭하여 페이지 이동시 사용자 경험을 향상시킬 수 있음
+  const queryClient = useQueryClient();
+
+  // currentPage가 바뀔 때마다 로직이 재실행 됨
+  // currentPage 변경으로 nextPage도 바뀌게 되고 그에 따라 prefetchQuery로 가져오는 nextPage 쿼리키도 바뀜
+  useEffect(() => {
+    // 범위 외의 데이터를 가져오지 않도록 조건 추가
+    if (currentPage < maxPostPage) {
+      const nextPage = currentPage + 1;
+      queryClient.prefetchQuery(['posts', nextPage], () =>
+        fetchPosts(nextPage)
+      );
+    }
+  }, [currentPage, queryClient]);
+
   // replace with useQuery
-  const data = [];
+  // const data = []; // 임시 하드코딩 데이터
+  // const { data, isLoading, error, isError } = useQuery('posts', fetchPosts, {
+  // const { data, isLoading, error, isError } = useQuery(
+  const { data, isLoading, error, isError, isFetching } = useQuery(
+    ['posts', currentPage],
+    () => fetchPosts(currentPage), // 주의: 인자를 들어갈 때는 반드시 무명함수로 작성
+    {
+      staleTime: 2000,
+      keepPreviousData: true, // 쿼리키가 바뀔때도 지난 데이터 유지(이전 페이지로 돌아갈 경우 해당 데이터가 캐시에 있도록)
+    }
+  );
+
+  // if (!data) return <div></div>; // 얼리 리턴 형태로 처리
+  if (isLoading) return <h3>Loading...</h3>; // 로딩상태 얼리 리턴으로 처리
+  // if (isFetching) return <h3>Fetching in progress...</h3>; // 캐시된 데이터의 존재여부와 상관없이 프리페치 전 매번 동작
+  if (isError)
+    // 에러상태 얼리 리턴으로 처리
+    return (
+      <>
+        <h3>Oops, somthing went wrong!</h3>
+        <p>{error.toString()}</p>
+      </>
+    );
 
   return (
     <>
@@ -31,11 +76,24 @@ export function Posts() {
         ))}
       </ul>
       <div className="pages">
-        <button disabled onClick={() => {}}>
+        {/* <button disabled onClick={() => {}}> */}
+        <button
+          disabled={currentPage <= 1}
+          onClick={() => {
+            setCurrentPage((prevValue) => prevValue - 1);
+          }}
+        >
           Previous page
         </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        {/* <span>Page {currentPage + 1}</span> */}
+        <span>Page {currentPage}</span>
+        {/* <button disabled onClick={() => {}}> */}
+        <button
+          disabled={currentPage >= maxPostPage}
+          onClick={() => {
+            setCurrentPage((prevValue) => prevValue + 1);
+          }}
+        >
           Next page
         </button>
       </div>
