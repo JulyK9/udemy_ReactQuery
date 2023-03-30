e676c3bae6d7e90bee3398951113f693b9f5a0dd
JKKIM <jkesonjk@gmail.com>
2023-03-30 10:48:06 +0900

    #3 feat: useInfiniteQuery 훅에서 isFetching, isLoading, isError 적용

--- base-infinite-swapi/src/people/InfinitePeople.jsx
+++ base-infinite-swapi/src/people/InfinitePeople.jsx
@@ -10,7 +10,15 @@ const fetchUrl = async (url) => {
 
 export function InfinitePeople() {
   // TODO: get data for InfiniteScroll via React Query
-  const { data, fetchNextPage, hasNextPage } = useInfiniteQuery(
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isLoading,
+    isError,
+    error,
+    isFetching,
+  } = useInfiniteQuery(
     'sw-people', // 쿼리 키
     ({ pageParam = initialUrl }) => fetchUrl(pageParam), // 쿼리 함수는 객체를 매개변수로 받음
     {
@@ -21,21 +29,29 @@ export function InfinitePeople() {
   );
   // 페이지를 계속 로드할 때 data에 데이터의 페이지가 포함됨
 
+  // isFetching으로 얼리 리턴을 주게 되면 새로운 페이지가 열릴 때마다 조기 반환을 해서 스크롤이 다시 위로 올라가버림
+  if (isLoading) return <div className="loading">Loading...</div>;
+  if (isError) return <div>Error!! {error.toString()}</div>;
+
   // return <InfiniteScroll />;
   return (
-    <InfiniteScroll loadMore={fetchNextPage} hasMore={hasNextPage}>
-      {data.pages.map((pageData) => {
-        return pageData.results.map((person) => {
-          return (
-            <Person
-              key={person.name}
-              name={person.name}
-              hairColor={person.hair_color}
-              eyeColor={person.eye_color}
-            />
-          );
-        });
-      })}
-    </InfiniteScroll>
+    <>
+      {/* 페칭 중에도 로딩 메시지 보여주기 */}
+      {isFetching && <div className="loading">Loading...</div>}
+      <InfiniteScroll loadMore={fetchNextPage} hasMore={hasNextPage}>
+        {data.pages.map((pageData) => {
+          return pageData.results.map((person) => {
+            return (
+              <Person
+                key={person.name}
+                name={person.name}
+                hairColor={person.hair_color}
+                eyeColor={person.eye_color}
+              />
+            );
+          });
+        })}
+      </InfiniteScroll>
+    </>
   );
 }
